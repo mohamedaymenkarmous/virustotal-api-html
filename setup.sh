b8790914d460c7cf84bd0a330daffebf8ca89c84
mohamedaymenkarmous <mohamed.aymen.karmous@gmail.com>
2019-11-24 22:45:48 +0100

    Fixing setup.sh
    output directory and config.json were not properly created

--- setup.sh
+++ setup.sh
@@ -3,5 +3,5 @@
 sudo apt-get update && \
 sudo apt-get -y install python3-pip && \
 sudo pip3 install -r requirements.txt
-ls -1 config.json || echo "ok"
-mkdir output
+ls -1 config.json 2> /dev/null || cp config.json.bak config.json
+mkdir output 2> /dev/null
